ec8c6d95cbed51a873cb93b9fecda38ae3e610af
Anders Bjerner <abjerner@skybrud.dk>
2021-03-09 16:08:29 +0100

    Updated package resources

---
+++ src/Skybrud.Umbraco.Time/App_Plugins/Skybrud.Time/Scripts/Controllers/TimeZone.js
@@ -0,0 +1,33 @@
+﻿angular.module("umbraco").controller("Skybrud.TimeZone.Controller", function ($scope, $http) {
+
+    const vm = this;
+
+    vm.loading = true;
+    vm.timeZone = null;
+    vm.timeZones = null;
+
+    vm.change = function () {
+        $scope.model.value = vm.timeZone.id;
+    };
+
+    $http.get("/umbraco/backoffice/Skybrud/Time/GetTimeZones").then(function (res) {
+
+        vm.loading = false;
+        vm.timeZones = res.data;
+
+        if ($scope.model.value) {
+            vm.timeZones.forEach(function(tz) {
+                if (tz.id === $scope.model.value) {
+                    vm.timeZone = tz;
+                }
+            });
+        }
+
+        if (!vm.timeZone) {
+            vm.timeZone = vm.timeZones.find(x => x.id === "local");
+            $scope.model.value = "local";
+        }
+
+    });
+
+});
