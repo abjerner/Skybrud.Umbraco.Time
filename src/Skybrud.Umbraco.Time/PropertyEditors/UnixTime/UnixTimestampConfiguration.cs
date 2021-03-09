137f2cfcc363e7c5da715ee838b2fed449179e7b
Anders Bjerner <abjerner@skybrud.dk>
2021-03-09 16:07:56 +0100

    Updated the UNIX time configuration a bit

--- src/Skybrud.Umbraco.Time/PropertyEditors/UnixTime/UnixTimestampConfiguration.cs
+++ src/Skybrud.Umbraco.Time/PropertyEditors/UnixTime/UnixTimestampConfiguration.cs
@@ -4,11 +4,14 @@ namespace Skybrud.Umbraco.Time.PropertyEditors.UnixTime {
         
     public class UnixTimestampConfiguration {
 
-        [ConfigurationField("readonly", "Readonly", "boolean", Description = "Specify whether the editor should be readonly.")]
-        public bool IsReadOnly { get; set; }
-
         [ConfigurationField("timeZone", "Time zone", "/App_Plugins/Skybrud.Time/Views/Editors/TimeZone.html", Description = "Select the time zone of the returned <strong>EssentialsTime</strong>. This does not affect the value saved in Umbraco.")]
         public string TimeZone { get; set; }
+
+        [ConfigurationField("showUnixTimestamp", "Show UNIX timestamp", "boolean", Description = "Show the UNIX timestamp in the editor.")]
+        public bool ShowUnixTimestampp { get; set; }
+
+        [ConfigurationField("readonly", "Readonly", "boolean", Description = "Specify whether the editor should be readonly.")]
+        public bool IsReadOnly { get; set; }
 
     }
 
