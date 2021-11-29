69b9b1ddf41bdfa6a2d97c457cea81a33a880ba8
Anders Bjerner <abjerner@skybrud.dk>
2021-11-29 14:33:15 +0100

    Replaced msbuild with dotnet build

--- release.bat
+++ release.bat
@@ -1,4 +1,2 @@
 @echo off
-cd src/Skybrud.Umbraco.Time
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe" /t:rebuild /t:pack /p:Configuration=Release /p:BuildTools=1 /p:PackageOutputPath=../../releases/nuget
-cd ../../
+dotnet build src/Skybrud.Umbraco.Time --configuration Release /t:rebuild /t:pack -p:BuildTools=1 -p:PackageOutputPath=../../releases/nuget
